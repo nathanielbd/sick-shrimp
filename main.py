5c2c15d97c39ae93c66d95f9de9d16e9cc7c34fb
nathanielbd <36861707+nathanielbd@users.noreply.github.com>
2019-02-03 11:10:48 -0600

    updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
-import numpy
+import numpy as np
+import cv2
+
 #import Image
 
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 # from flask.ext.sqlalchemy import SQLAlchemy
 # referenced from "Pretty Printed"
 app = Flask(__name__)
@@ -14,10 +17,10 @@ def page():
 @app.route("/upload", methods = ["POST"])
 def upload():
 	file = request.files["inputFile"]
+
 	from keras.models import Sequential
 	from keras.layers import Conv2D, MaxPooling2D
 	from keras.layers import Activation, Dropout, Flatten, Dense
-	import os
 	from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 	model = Sequential()
@@ -36,9 +39,32 @@ def upload():
 				metrics=['accuracy'])
 
 	model.load_weights('try_one.h5')
-	array = img_to_array(file)
+    
+	# path = '/UPLOAD_FOLDER'
+	# os.mkdir(path)
+	# app.config['UPLOAD_FOLDER'] = path
+
+	path = '/UPLOAD_FOLDER'
+	app.config['UPLOAD_FOLDER'] = path
+
+	filename = secure_filename(file.filename) # save file 
+	filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+	file.save(filepath)
+
+	img = cv2.imread(filepath)
+	img = cv2.resize(img, (100, 300))
+	print(img)
+	img = numpy.reshape(img, [1, 100, 300, 3])
+	classes = model.predict_classes(img)
+	# os.chown(path, uid, gid)
+
+	#file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+
+	#img = load_img('./'+file.filename, target_size=(100, 300))
+	
+	#array = img_to_array(file)
 	#result = model.predict(array, batch_size = 1, verbose = 0)
-	return "model works"
+	return classes
 
 
 if __name__ == "__main__":
